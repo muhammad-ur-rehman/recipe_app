f3ec9796efcdc8cc2ff35271d6dbc9fd1bb8fe6c
= <=>
2022-06-24 00:05:01 +0500

    default route setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+
+  root :to => redirect('/api-docs')
+
   resources :meals, only: %i(index show)
   resources :categories, only: %i(index)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
